1eee187e262ed81c040bcab778386495705afb6a
Kris Wilson <krispykrisp12@gmail.com>
2018-09-20 18:11:03 -0400

    backing up

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let logger = require("morgan");
 let mongoose = require("mongoose");
-// let exphbs = require("express-handlebars");
+
 let axios = require("axios");
 let request = require("request");
 let cheerio = require("cheerio");
+
 // ============================================
-let PORT = 3000;
+let path = require("path");
+let PORT = process.env.PORT || 3000;
 let db = require("./models");
 let app = express();
 // ============================================
@@ -16,56 +18,91 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+// ============================================
 // connect to the Mongo DB
-mongoose.connect("mongodb://localhost/News", { useNewUrlParser: true });
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/News";
 
-// ============================================
-// app.use(express.static("public"));
-// app.engine("handlebars", exphbs({
-//     defaultLayout: "main"
-// }));
-// app.set("view engine", "handlebars");
-// ============================================
-let url = "https://www.nytimes.com/section/food";
-// Making a request for `nhl.com`'s homepage
-request(url, function(error, response, html) {
-  // Load the body of the HTML into cheerio
-  let $ = cheerio.load(html);
-
-  $("article.story").each(function(i, element) {
-    let results = {};
-      
-    let headline = $(element).find("h2.headline").text();
-    let link = $(element).find("a").attr("href");
-    let summary = $(element).find("p.summary").text();
-    let author = $(element).find("p.byline").text();
-    
-    // Make an object with data we scraped from the N.Y. Times
-    results.push({
-      headline: headline,
-      link: link,
-      summary: summary,
-      author: author
-    
-    });
-  });
-
-  // After looping through each article.story log the results
-  // console.log(results);
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true 
+})
+.then(connection => {
+    console.log('Connected to MongoDB');
+})
+.catch(err => {
+    console.log(err.message);
 });
 
-app.get("/", function(req, res) {
-	Article.find({}, null, {sort: {created: -1}}, function(err, data) {
-		if(data.length === 0) {
-			res.render("placeholder", {message: "There's nothing scraped yet. Please click \"Scrape For Newest Articles\" for fresh and delicious news."});
-		}
-		else{
-			res.render("index", {articles: data});
-		}
-	});
+// ============================================
+// ============================================
+// Use express.static to serve the public folder as a static directory
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
+// ============================================
+// Use express.static to serve the public folder as a static directory
+// Handlerbars  
+let exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+// ============================================
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+// ============================================
+app.get("/", function (req, res) {
+  db.Article.find({})
+    .then(function (articles) {
+      res.render("index", {
+        articles: articles
+      });
+    })
+    .catch(function (err) {
+      res.json(err);
+    })
+})
+// ============================================
+
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  axios.get("https://www.nytimes.com/section/food").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    let $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      let result = {};
+    
+      // Add the text and href of every link, and save them as properties of the result object
+      result.headline = $(this)
+        .find("h2.headline").text();
+      result.link = $(this)
+        .find("a").attr("href");
+      result.summary = $(this)
+        .find("p.summary").text();
+      result.author = $(this)
+        .find("p.byline").text();
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
+  });
 });
 
 // Start the server
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+    console.log(`App running on port ${PORT} !`);
   });
